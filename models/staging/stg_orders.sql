07f0a0a89853064834c5dd2ed44e8d167f65886e
osmar11692@gmail.com <osmar11692@gmail.com>
2022-12-01 01:00:30 +0000

    Desarrollando stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,20 @@
+select
+--Estos campos vienen de raw orders
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ORDERSELLINGPRICE,
+o.ORDERCOSTPRICE,
+o.ORDERSELLINGPRICE - o.ORDERCOSTPRICE as ORDERPROFIT,
+--Estos campos vienen de raw customer
+c.customername,
+c.segment,
+c.country,
+--Estos campos vienen de raw product
+p.category,
+p.productname,
+p.subcategory
+FROM {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c on o.customerid = c.customerid
+left join {{ ref('raw_product') }} as p on o.productid = p.productid
